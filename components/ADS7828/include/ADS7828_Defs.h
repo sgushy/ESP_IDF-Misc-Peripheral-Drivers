c81ee046c20016c271564d46ca6cf4c0832047e8
sgushy <113747791+sgushy@users.noreply.github.com>
2024-11-26 12:33:57 -0800

    Update ADS7828_Defs.h

--- components/ADS7828/include/ADS7828_Defs.h
+++ components/ADS7828/include/ADS7828_Defs.h
@@ -9,6 +9,9 @@
 
 #define ADS7828_CONV_FULL_ADDR(x) (x)      // Convert 0x20 etc. address to full based on datasheet
 
+#define ADS7828_12_BIT_RESOLUTION 2
+#define ADS7830_8_BIT_RESOLUTION 1
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -21,6 +24,7 @@ extern "C"
         int i2c_sda;
         int i2c_scl;
         int i2c_freq_hz;
+        int data_bit_resolution; 
         int status;
     } ads7828_config_t;
 
