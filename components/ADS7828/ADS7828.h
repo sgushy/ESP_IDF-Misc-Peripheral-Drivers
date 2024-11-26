57daa698f858812143a971bb9ed0efe70df3a53e
sgushy <113747791+sgushy@users.noreply.github.com>
2024-11-26 12:34:11 -0800

    Update ADS7828.h

--- components/ADS7828/ADS7828.h
+++ components/ADS7828/ADS7828.h
@@ -14,8 +14,7 @@ extern "C"
 int ads7828_init(const ads7828_config_t *ads7828_handle);
 int ads7830_init(const ads7828_config_t *ads7828_handle);
 
-int ads7828_read_channel_16(const ads7828_config_t *ads7828_handle, int channel_num, uint16_t *buffer);
-int ads7830_read_channel_8(const ads7828_config_t *ads7828_handle, int channel_num, uint8_t *buffer);
+int ads7828_read_channel(const ads7828_config_t *ads7828_handle, int channel_num, int *buffer);
 
 #ifdef __cplusplus
 }
