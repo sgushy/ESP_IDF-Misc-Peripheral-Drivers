d6fc67e6b42dbb62f637aaab4d4d66f7a99601f1
sgushy <113747791+sgushy@users.noreply.github.com>
2024-11-26 12:30:16 -0800

    Update ADS7828.h with function definitions

--- components/ADS7828/ADS7828.h
+++ components/ADS7828/ADS7828.h
@@ -3,3 +3,20 @@
 ///
 
 #pragma once
+
+#include "ADS7828_Defs.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int ads7828_init(const ads7828_config_t *ads7828_handle);
+int ads7830_init(const ads7828_config_t *ads7828_handle);
+
+int ads7828_read_channel_16(const ads7828_config_t *ads7828_handle, int channel_num, uint16_t *buffer);
+int ads7830_read_channel_8(const ads7828_config_t *ads7828_handle, int channel_num, uint8_t *buffer);
+
+#ifdef __cplusplus
+}
+#endif
